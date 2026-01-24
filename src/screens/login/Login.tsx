e6532fdff6d60d5e34a0a9fdc6869b4ebbdc6440
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-24 19:12:54 +0530

    refactor: implement custom login form

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,96 +1,21 @@
-import React, { useState } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import {
-  ActivityIndicator,
-  Alert,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import { userLogin } from '@services/auth.service';
-import { COLORS } from '@constants/colors';
-import CustomButton from '@components/button';
+import React from 'react';
+import { View } from 'react-native';
+import { APP_INFO } from '@constants/constants';
+import { useLogin } from 'hooks/useLogin';
 import { styles } from './styles';
+import LoginForm from '@components/loginForm/LoginForm';
 
 export default function LoginScreen() {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({
-    userName: false,
-    password: false,
-  });
-
-  const {mutate, isPending } = useMutation({
-    mutationFn: userLogin,
-    onError: (error: Error) => {
-      Alert.alert('Login Failed', error.message , [
-        { text: 'OK' },
-      ]);
-    },
-    onSuccess: () => {
-      Alert.alert('Success', 'Logged in successfully!');
-    },
-  });
-
-  const loginHandle = () => {
-    const userNameError = userName.trim() === '';
-    const passwordError = password.trim() === '';
-
-    setErrors({
-      userName: userNameError,
-      password: passwordError,
-    });
-
-    if (!userNameError && !passwordError) {
-      mutate({ userName, password });
-    }
-  };
+  const { mutate, isPending } = useLogin();
 
   return (
     <View style={styles.container}>
-
-      <View style={styles.card}>
-        <Text style={styles.title}>Movie Explorer</Text>
-        <Text style={styles.subtitle}>Login to continue</Text>
-
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={[
-              styles.input,
-              errors.userName && styles.inputError,
-            ]}
-            placeholder="Username"
-            placeholderTextColor={COLORS.INPUT_PLACEHOLDER}
-            value={userName}
-            onChangeText={setUserName}
-            autoCapitalize="none"
-            editable={!isPending}
-          />
-          {errors.userName && (
-            <Text style={styles.errorText}>Please enter a valid username</Text>
-          )}
-        </View>
-
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={[
-              styles.input,
-              errors.password && styles.inputError,
-            ]}
-            placeholder="Password"
-            placeholderTextColor={COLORS.INPUT_PLACEHOLDER}
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-            editable={!isPending}
-          />
-          {errors.password && (
-            <Text style={styles.errorText}>Please enter your password</Text>
-          )}
-        </View>
-
-        {!isPending ? <CustomButton title='Login' onPress={loginHandle}/> : <ActivityIndicator size={'small'}/>}
-      </View>
+      <LoginForm
+        title={APP_INFO.TITLE}
+        subtitle={APP_INFO.LOGIN_SUBTITLE}
+        onSubmit={mutate}
+        isPending={isPending}
+      />
     </View>
   );
 }

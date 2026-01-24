5a4924248ced7e41032b7c71937c25caf25c2569
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-24 19:11:45 +0530

    refactor: provide fromatting to store

--- src/store/userSession.ts
+++ src/store/userSession.ts
@@ -1,18 +1,21 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-export const useUserSession = create<UserSession>()(persist(set=>({
-    sessionId: "",
-    isLoggedIn: false
-}),
-{
-    name: 'user-session-storage',
-    storage: createJSONStorage(()=>AsyncStorage),
-    onRehydrateStorage: () => {
+export const useUserSession = create<UserSession>()(
+  persist(
+    set => ({
+      sessionId: '',
+      isLoggedIn: false,
+    }),
+    {
+      name: 'user-session-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => {
         return () => {
           useUserSession.setState({ isLoggedIn: true });
         };
-}}
-
-))
+      },
+    },
+  ),
+);

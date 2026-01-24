a6f9957492218305b80a51eb075d6436f300d620
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-24 19:13:45 +0530

    refactor: rename axios intance file to camel case

--- src/network/axiosinstance.ts
+++
@@ -1,9 +0,0 @@
-import { ACCESS_TOKEN, BASE_URL } from "@env";
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`
-    }
-})

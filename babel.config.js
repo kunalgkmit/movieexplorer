69794f0dbdb3928cd1f7a8b072be7f721085c193
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-24 15:06:54 +0530

    feat: implement alias imports

--- babel.config.js
+++ babel.config.js
@@ -9,5 +9,23 @@ module.exports = {
         allowUndefined: false,
       },
     ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@constants': './src/constants',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@network': './src/network',
+          '@assets': './src/assets',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+      },
+    ],
   ],
 };

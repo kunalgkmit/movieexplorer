fdaf7b99319f3579fd235340cb89eca031777949
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-25 20:21:33 +0530

    refactor: remove logout button from hoem and profile

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,17 +1,9 @@
-import { useUserSession } from "@store/userSession";
-import { Button, Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
-export default function Home(){
-    const logoutHandle = ()=>{
-        useUserSession.setState({
-            isLoggedIn: false,
-        })
-        useUserSession.persist.clearStorage();
-    }
-    return(
-        <View style={{flex:1,alignItems:'center', justifyContent:'center'}}>
-            <Text>HOME SCREEN</Text>
-            <Button title="Logout" onPress={logoutHandle}/>
-        </View>
-    );
+export default function Home() {
+  return (
+    <View>
+      <Text>HOME SCREEN</Text>
+    </View>
+  );
 }

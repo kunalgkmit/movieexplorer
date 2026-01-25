2d60a9652a24ff308b6c01d54a19da0a243f7492
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-25 12:32:28 +0530

    refactor: move store types in store.d.ts

--- src/store/userSession.ts
+++ src/store/userSession.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-export const useUserSession = create<UserSession>()(
+export const useUserSession = create<UserSessionStore>()(
   persist(
     set => ({
       sessionId: '',
@@ -11,11 +11,6 @@ export const useUserSession = create<UserSession>()(
     {
       name: 'user-session-storage',
       storage: createJSONStorage(() => AsyncStorage),
-      onRehydrateStorage: () => {
-        return () => {
-          useUserSession.setState({ isLoggedIn: true });
-        };
-      },
     },
   ),
 );

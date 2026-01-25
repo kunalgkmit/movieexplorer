8f30600a40c7a1c69707fefc305711c044fdbbcb
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-25 14:42:45 +0530

    feat: implement loader inside custom button

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -6,14 +6,15 @@ interface LoginFormProps {
 interface ButtonProps {
   title: string;
   onPress: () => void;
+  isPending?: boolean;
 }
 
 interface CustomTextInputProps {
   placeholder: string;
   value: string;
-  onChangeText: (data:string) => void;
+  onChangeText: (data: string) => void;
   error?: string;
   editable?: boolean;
   autoCapitalize?: 'none';
-  secureTextEntry?: boolean
+  secureTextEntry?: boolean;
 }

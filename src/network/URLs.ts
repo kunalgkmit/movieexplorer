65743d864d6a768ae56cfe8f651e3840cb7b32a7
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-26 23:13:39 +0530

    refactor: remove specific movie api endpoint with common url

--- src/network/URLs.ts
+++ src/network/URLs.ts
@@ -6,7 +6,6 @@ export const ENDPOINTS = {
   },
 
   MOVIES: {
-    POPULARITY_DESC:
-      '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+    DISCOVER: '/discover/movie',
   },
 };

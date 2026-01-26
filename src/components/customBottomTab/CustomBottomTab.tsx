7d176a44e5e653ac82167c7aa36d901827112cc4
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-26 14:36:48 +0530

    feat: implement vector icons in custom bottom tabs

--- src/components/customBottomTab/CustomBottomTab.tsx
+++ src/components/customBottomTab/CustomBottomTab.tsx
@@ -1,43 +1,44 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { ROUTES } from '@constants/routes';
 import { styles } from './styles';
 import { COLORS } from '@constants/colors';
-
-const homeImg = require('@assets/images/home.png');
-const favouritesImg = require('@assets/images/favourite.png');
-const profileImg = require('@assets/images/avatar.png');
+import { ICONS } from '@constants/constants';
 
 export default function CustomBottomTab({
   state,
   navigation,
 }: BottomTabBarProps) {
+  const getIconName = (routeName: string, isFocused: boolean) => {
+    if (routeName === ROUTES.TABS.HOME) {
+      return isFocused ? ICONS.HOME : ICONS.HOME_OUTLINE;
+    } else if (routeName === ROUTES.TABS.FAVOURITES) {
+      return isFocused ? ICONS.HEART : ICONS.HEART_OUTLINE;
+    } else {
+      return isFocused ? ICONS.ACCOUNT : ICONS.ACCOUNT_OUTLINE;
+    }
+  };
+
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       {state.routes.map((item, index) => {
         const isFocused = state.index === index;
+
+        const iconName = getIconName(item.name, isFocused);
+
         const color = isFocused ? COLORS.PRIMARY : COLORS.TEXT_SECONDARY;
+
         return (
           <TouchableOpacity
             key={item.key}
             style={styles.button}
             onPress={() => navigation.navigate(item.name)}
           >
-            <View>
-              <Image
-                style={[styles.image, { tintColor: color }]}
-                source={
-                  item.name === ROUTES.TABS.HOME
-                    ? homeImg
-                    : item.name === ROUTES.TABS.FAVOURITES
-                    ? favouritesImg
-                    : profileImg
-                }
-              />
-            </View>
+            
+            <Icon name={iconName} size={30} color={color} />
+
             <Text style={[styles.title, { color: color }]}>{item.name}</Text>
           </TouchableOpacity>
         );

675178e31cb50bc3f1a00a1d592958607ca9efb5
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-26 23:11:54 +0530

    refactor: implement reusable logic for fetching movies

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,12 +1,34 @@
-import { axiosInstance } from "@network/axiosInstance";
-import { ENDPOINTS } from "@network/URLs";
+import { CLIENT_ERRORS } from '@constants/network';
+import { axiosInstance } from '@network/axiosInstance';
+import { ENDPOINTS } from '@network/URLs';
 
-export default async function fetchMovies(){
-    try {
-        const response = await axiosInstance.get(ENDPOINTS.MOVIES.POPULARITY_DESC);
-        // console.log("Popular Movies>>",response.data)
-        return response.data
-    } catch (error) {
-        
+export default async function fetchMovies({
+  pageParam,
+  sortBy,
+  includeVideo,
+  language,
+}: DefaultMovieParams) {
+  try {
+    const response = await axiosInstance.get(ENDPOINTS.MOVIES.DISCOVER, {
+      params: {
+        page: pageParam,
+        sort_by: sortBy,
+        include_adult: false,
+        include_video: includeVideo,
+        language: language,
+      },
+    });
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      const { status } = error.response;
+      if (status === CLIENT_ERRORS.UNAUTHORIZED) {
+        throw new Error("Unauthorized, can't access movies.");
+      }
+    } else {
+      throw new Error(
+        'Server is not reachable. Please check your internet connection.',
+      );
     }
+  }
 }

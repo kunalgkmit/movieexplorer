3da2207f90aca0cea7439ad467fd18293969c7cd
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-28 14:22:06 +0530

    refactor: modify movies with data having favourites condition

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,9 +3,12 @@ import { ActivityIndicator, Alert, FlatList, View } from 'react-native';
 import MovieCard from '@components/movieCard';
 import { styles } from './styles';
 import { useMovies } from '@hooks/useMovies';
+import { useFavMovies } from '@store/favourites';
+import { useMemo } from 'react';
 
 export default function Home() {
-  
+  const favMoviesIds = useFavMovies(state => state.favMoviesIds);
+
   const {
     data,
     isLoading,
@@ -20,7 +23,22 @@ export default function Home() {
     language: 'en-US',
   });
 
-  const movies = data?.pages?.map(page => page.results).flat() ?? [];
+  const movies = useMemo(() => {
+    const flattedData = data?.pages?.map(page => page.results).flat();
+
+    return flattedData?.map(movieItem => {
+      const findFavouriteMovie = favMoviesIds.find(fav => movieItem.id === fav);
+
+      return {
+        id: movieItem.id,
+        posterPath: movieItem.poster_path,
+        title: movieItem.title,
+        releaseDate: movieItem.release_date,
+        rating: movieItem.vote_average,
+        isFav: findFavouriteMovie ? true : false,
+      };
+    });
+  }, [favMoviesIds, data]);
 
   const loadMore = () => {
     if (hasNextPage && !isFetchingNextPage) {
@@ -49,19 +67,19 @@ export default function Home() {
         columnWrapperStyle={styles.columnWrapper}
         renderItem={({ item }) => (
           <MovieCard
-            posterPath={item.poster_path}
+            movieId={item.id}
+            posterPath={item.posterPath}
             title={item.title}
-            releaseDate={item.release_date}
-            rating={item.vote_average}
+            releaseDate={item.releaseDate}
+            rating={item.rating}
+            checkFav={item.isFav}
           />
         )}
         keyExtractor={item => item.id}
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         ListFooterComponent={
-          isFetchingNextPage ? (
-              <ActivityIndicator size="small" />
-          ) : null
+          isFetchingNextPage ? <ActivityIndicator size="small" /> : null
         }
       />
     </View>

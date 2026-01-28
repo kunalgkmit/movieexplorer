bf5a07fd5ce977698f76ccce3333b1ab96f7207e
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-28 14:19:20 +0530

    feat: implement favourites screen

--- src/screens/favourites/Favourites.tsx
+++ src/screens/favourites/Favourites.tsx
@@ -1,9 +1,31 @@
-import { Text, View } from "react-native";
+import { useFavMovies } from '@store/favourites';
+import { FlatList, Text, View } from 'react-native';
+import { styles } from './styles';
+import MovieCard from '@components/movieCard';
 
-export default function Favourites(){
-    return(
-        <View>
-            <Text>Favourites SCREEN</Text>
-        </View>
-    );
+export default function Favourites() {
+  const favourites = useFavMovies(state => state.favourites);
+  const isFavourite = useFavMovies(state => state.isFavourite);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={favourites}
+        numColumns={2}
+        contentContainerStyle={styles.listContent}
+        columnWrapperStyle={styles.columnWrapper}
+        renderItem={({ item }) => (
+          <MovieCard
+            movieId={item.id}
+            posterPath={item.poster_path}
+            title={item.title}
+            releaseDate={item.release_date}
+            rating={item.rating}
+            checkFav={isFavourite(item.id)}
+          />
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
+  );
 }
---
+++ src/screens/favourites/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '@constants/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BG_PRIMARY,
+  },
+
+  listContent: {
+    padding: 15,
+  },
+
+  columnWrapper: {
+    justifyContent:'space-around',
+  },
+});

90d64587381b38db24ea1c4eb0d062c7db044a14
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-30 02:01:08 +0530

    feat: implemented sort by functionality

---
+++ src/components/sortByOptions/SortByOptions.tsx
@@ -0,0 +1,46 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+
+interface SortByProps {
+  setSortBy: (params: string) => void;
+  toggleSort: () => void;
+}
+
+export default function SortByOptions({ setSortBy, toggleSort }: SortByProps) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          setSortBy('vote_count.desc');
+          toggleSort();
+        }}
+      >
+        <Text style={styles.text}>High Rated</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setSortBy('vote_count.asc');
+          toggleSort();
+        }}
+      >
+        <Text style={styles.text}>Low Rated</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setSortBy('primary_release_date.desc');
+          toggleSort();
+        }}
+      >
+        <Text style={styles.text}>Upcoming Movies</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setSortBy('primary_release_date.asc');
+          toggleSort();
+        }}
+      >
+        <Text style={styles.text}>Old Movies</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/components/sortByOptions/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+        gap:4
+    },
+    text:{
+        fontSize:15,
+    }
+})

3964731c4ba5e1ff6519420c55fd3b9fb9688537
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-01-30 17:25:46 +0530

    feat add status bar in login screen

--- src/screens/login/Login.tsx
+++ src/screens/login/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 import { APP_INFO } from '@constants/constants';
 import LoginForm from '@components/loginForm/LoginForm';
@@ -9,6 +9,8 @@ import { styles } from './styles';
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
+            <StatusBar barStyle={'dark-content'} />
+      
       <LoginForm title={APP_INFO.TITLE} subtitle={APP_INFO.LOGIN_SUBTITLE} />
     </View>
   );

2375a76a8d14890d6e9e31ffe810cd44dd8b1427
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-01 11:34:02 +0530

    refactor: put activity indicator inside of app bar

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from 'react-native';
 
+import { COLORS } from '@constants/colors';
 import CustomAppBar from '@components/customAppBar/CustomAppBar';
 import CustomButton from '@components/button';
 import CustomActivityIndicator from '@components/customActivityIndicator';
@@ -7,7 +8,6 @@ import { useUserDetails } from '@hooks/useUserDetails';
 import { useUserSession } from '@store/userSession';
 
 import { styles } from './styles';
-import { COLORS } from '@constants/colors';
 
 export default function Profile() {
   const { data, isLoading } = useUserDetails();
@@ -19,22 +19,26 @@ export default function Profile() {
     });
   };
 
-  if (isLoading) {
-    return <CustomActivityIndicator color={COLORS.SHADOW} />;
-  }
-
   return (
     <>
       <CustomAppBar title="Profile" />
-      <View style={styles.container}>
-        <View style={styles.avatarBG}>
-          <Text style={styles.avatarInitials}>{data.name[0]}</Text>
+      {isLoading ? (
+        <CustomActivityIndicator color={COLORS.SHADOW} />
+      ) : (
+        <View style={styles.container}>
+          <View style={styles.avatarBG}>
+            <Text style={styles.avatarInitials}>{data.name[0]}</Text>
+          </View>
+          <Text style={styles.name}>{data.name}</Text>
+          <View style={styles.buttonStyle}>
+            <CustomButton
+              title="Logout"
+              onPress={handleLogout}
+              isLogout={true}
+            />
+          </View>
         </View>
-        <Text style={styles.name}>{data.name}</Text>
-        <View style={styles.buttonStyle}>
-          <CustomButton title="Logout" onPress={handleLogout} isLogout={true} />
-        </View>
-      </View>
+      )}
     </>
   );
 }

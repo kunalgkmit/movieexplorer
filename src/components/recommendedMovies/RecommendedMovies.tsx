a6a402687952b9e47c06d03ca853050e28afca89
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-03 17:17:46 +0530

    refactor: handle movie data formatting in hook itself

--- src/components/recommendedMovies/RecommendedMovies.tsx
+++ src/components/recommendedMovies/RecommendedMovies.tsx
@@ -1,25 +1,17 @@
-import MovieCard from '@components/movieCard';
 import { FlatList, Text, View } from 'react-native';
-import { styles } from './styles';
-import { useMemo } from 'react';
 import { useRecommendedMovies } from '@hooks/useRecommendedMovies';
-import { formatMovieData } from '@utils/helpers';
-import { useFavMoviesStore } from '@store/favourites';
+import MovieCard from '@components/movieCard';
+
+import { styles } from './styles';
 
 export default function RecommendedMovies({movieId}: {movieId?:number}) {
-  const favMovieIds = useFavMoviesStore(state => state.favMoviesIds);
-  const isFavourite = useFavMoviesStore(state => state.isFavourite);
 
   const { data: recommendedMovies } = useRecommendedMovies(movieId);
 
-  const movies = useMemo(
-    () => formatMovieData(recommendedMovies ?? [], isFavourite),
-    [recommendedMovies, favMovieIds],
-  );
   return (
     <FlatList
       ListEmptyComponent={<Text>No Recommended Movies</Text>}
-      data={movies}
+      data={recommendedMovies}
       horizontal={true}
       contentContainerStyle={styles.contentContainer}
       showsHorizontalScrollIndicator={false}
--- src/hooks/useRecommendedMovies.ts
+++ src/hooks/useRecommendedMovies.ts
@@ -1,11 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { fetchRecommendedMovies } from '@services/movies.service';
+import { formatMovieData } from '@utils/helpers';
+import { useFavMoviesStore } from '@store/favourites';
 
 export const useRecommendedMovies = (movieId?: number) => {
+  const favMovieIds = useFavMoviesStore(state => state.favMoviesIds);
+  const isFavourite = useFavMoviesStore(state => state.isFavourite);
   return useQuery({
     queryKey: ['RecommendedMovies', movieId],
     queryFn: () => fetchRecommendedMovies(movieId),
-    select: data => data?.results ?? [],
+    select: data => {
+      const recommendedMovies = data?.results ?? [];
+
+      return formatMovieData(recommendedMovies, isFavourite);
+    },
   });
 };

ef995b315be290e5ec49b8b28a49741b8806da92
Kunal Prajapat <kunalprajapat@gkmit.co>
2026-02-03 18:56:16 +0530

    refactor: make recommended movie component responsive

--- src/components/recommendedMovies/RecommendedMovies.tsx
+++ src/components/recommendedMovies/RecommendedMovies.tsx
@@ -1,31 +1,34 @@
 import { FlatList, Text, View } from 'react-native';
 import { useRecommendedMovies } from '@hooks/useRecommendedMovies';
+
 import MovieCard from '@components/movieCard';
 
 import { styles } from './styles';
 
-export default function RecommendedMovies({movieId}: {movieId?:number}) {
-
+export default function RecommendedMovies({ movieId }: { movieId?: number }) {
   const { data: recommendedMovies } = useRecommendedMovies(movieId);
 
   return (
-    <FlatList
-      ListEmptyComponent={<Text>No Recommended Movies</Text>}
-      data={recommendedMovies}
-      horizontal={true}
-      contentContainerStyle={styles.contentContainer}
-      showsHorizontalScrollIndicator={false}
-      renderItem={({ item }) => (
-        <View style={styles.movieCardWrapper}>
-          <MovieCard
-            movieDetails={item}
-            height={320}
-            width={150}
-            posterHeight={230}
-          />
-        </View>
-      )}
-      keyExtractor={item => item.movieId}
-    />
+    <>
+      <View style={styles.recommendedText}>
+        <Text style={styles.title}>Recommended Movies</Text>
+      </View>
+
+      <FlatList
+        ListEmptyComponent={<Text>No Recommended Movies</Text>}
+        data={recommendedMovies}
+        horizontal={true}
+        contentContainerStyle={styles.contentContainer}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.movieCardWrapper}>
+            <MovieCard
+              movieDetails={item}
+            />
+          </View>
+        )}
+        keyExtractor={item => item.movieId}
+      />
+    </>
   );
 }
